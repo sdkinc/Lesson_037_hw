eed02207a059ed60c209034761274ea4de52c45e
Olexander Zhurba <starkyzh@gmail.com>
2023-05-09 16:08:18 +0200

    add class Human

--- src/Human.java
+++ src/Human.java
@@ -1,6 +1,39 @@
 public class Human {
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  /*
+    Создайте класс "Человек", конструктор которого принимает имя.
+    При попытке вывести объект этого класса на экран должна выводиться строка "Человек по имени ...".
+    Добавьте в класс "Человек" метод "приветствие", который будет выводить строку: "Я родился!"
+    Создайте классы "Мужчина" и "Женщина", которые будут наследниками класса "Человек".
+    Попытка вывести их на экран должна приводить к выводу строки "Мужчина по имени ..."
+    или "Женщина по имени ..."
+    Метод "приветствие" класса "Женщина" должен выводить строку: "Я родилась!"
+   */
+  private String name;
+  private String type;
+
+  public Human(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  protected void setType(String type) {
+    this.type = type;
+  }
+
+  public String greet() {
+    return "i was born!";
+  }
+
+  @Override
+  public String toString() {
+    return type + " with name " + getName();
   }
 }
